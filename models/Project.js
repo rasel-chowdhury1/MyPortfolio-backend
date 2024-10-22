9f45c71685665961e0e11684260328cf44e67bb1
rasel-chowdhury1 <rasel.chowdhury0001@gmail.com>
2024-10-22 11:54:43 +0600

    done

--- models/Project.js
+++ models/Project.js
@@ -2,8 +2,10 @@ const mongoose = require('mongoose');
 
 const projectSchema = new mongoose.Schema({
   title: { type: String, required: true },
-  description: { type: String, required: true },
-  link: { type: String, required: true },
+  des: { type: String, required: true },
+  img: { type: String, required: true },
+  liveLink: { type: String, required: true },
+  githubLink: { type: String, required: true },
 }, { timestamps: true });
 
 module.exports = mongoose.model('Project', projectSchema);
--- routes/projects.js
+++ routes/projects.js
@@ -11,10 +11,17 @@ router.get('/', async (req, res) => {
 });
 
 // Create a project
-router.post('/', authMiddleware, async (req, res) => {
-  const { title, description, link } = req.body;
-  const project = await Project.create({ title, description, link });
+router.post('/',  async (req, res) => {
+  const { title, des, img, liveLink, githubLink} = req.body;
+  const project = await Project.create({ title, des, img, liveLink, githubLink });
   res.status(201).json(project);
+});
+// delete a project
+router.delete('/',  async (req, res) => {
+  const { _id } = req.body;
+  const result = await Project.findByIdAndDelete(_id);
+  console.log({result})
+  res.status(201).json(result);
 });
 
 module.exports = router;
